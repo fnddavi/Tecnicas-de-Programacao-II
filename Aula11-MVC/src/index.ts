fbd96e1f9f4c1ff6a780ed4ab282e4319596a639
Fernando Davi <92491040+fnddavi@users.noreply.github.com>
2024-04-06 23:48:15 -0300

    Aula11-MVC

---
+++ Aula11-MVC/src/index.ts
@@ -0,0 +1,50 @@
+class TarefaModel {
+  id: number;
+  descricao: string;
+  concluida: boolean;
+  constructor(id: number, descricao: string, concluida: boolean = false) {
+    this.id = id;
+    this.descricao = descricao;
+    this.concluida = concluida;
+  }
+}
+
+class TarefaView {
+  mostrar(tarefas: TarefaModel[]) {
+    for (const tarefa of tarefas) {
+      console.log(`${tarefa.id}: ${tarefa.descricao} -
+    ${tarefa.concluida ? "Concluída" : "Pendente"}`);
+    }
+  }
+}
+
+class TarefaController {
+  private modelo: TarefaModel[];
+  private visao: TarefaView;
+  constructor(modelo: TarefaModel[], visao: TarefaView) {
+    this.modelo = modelo;
+    this.visao = visao;
+  }
+  addTarefa(descricao: string) {
+    const novaTarefa = new TarefaModel(this.modelo.length + 1, descricao);
+    this.modelo.push(novaTarefa);
+    this.visao.mostrar(this.modelo);
+  }
+  completaTask(id: number) {
+    const tarefa = this.modelo.find((tarefa) => tarefa.id === id);
+    if (tarefa) {
+      tarefa.concluida = true;
+    }
+    this.visao.mostrar(this.modelo);
+  }
+}
+const tarefas: TarefaModel[] = [];
+const tarefav = new TarefaView();
+const tarefac = new TarefaController(tarefas, tarefav);
+tarefac.addTarefa("Fazer algo");
+console.log("-----------------------");
+tarefac.addTarefa("Fazer outra coisa");
+console.log("-----------------------");
+tarefac.addTarefa("Fazer mais alguma coisa");
+console.log("-----------------------");
+tarefac.completaTask(1);
